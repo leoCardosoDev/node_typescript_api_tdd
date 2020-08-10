354475f8748a28936224710057b66a1d9a6dac47
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-10 18:36:53 -0300

    chore: add prettier

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,8 +1,8 @@
-import { SetupServer } from '@src/server'
-import supertest from 'supertest'
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
 
 beforeAll(async () => {
-  const server = new SetupServer()
-  server.init()
-  global.testRequest = supertest(server.getApp())
-})
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp());
+});
