a3bde9987e6f844d304e6dba2d587bae8f5c2102
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-11 17:04:03 -0300

    test: Refactor Should return the normalized forecast from the StormGlass service with axios

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -2,7 +2,7 @@ import { AxiosStatic } from 'axios'
 
 export class StormGlass {
   readonly stormGlassAPIParams = 
-  'swellDirection, swellHeight, swellPeriod, waveDirection, waveHeight, windDirection'
+  'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
   readonly stormGlassAPISource = 'noaaa'
   constructor(protected request: AxiosStatic) {}
   public async fetchPoints(lat: number, lng: number): Promise<any> {
