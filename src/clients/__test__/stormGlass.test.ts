c3fa59f5f56f591721ed3a08769622945dd1b06d
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-11 18:57:57 -0300

    test: Should return the normalized forecast from the StormGlass service (5 ms)

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,20 +1,22 @@
 import { StormGlass } from '@src/clients/stormGlass'
 import axios from 'axios'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_3_hours.json'
+import stormGlassNormalized3hoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
 
 jest.mock('axios')
 
 describe('StromGlass', () => {
-  
+
   it('Should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726
     const lng = 151.289824
 
-    axios.get = jest.fn().mockResolvedValue({})
+    axios.get = jest.fn().mockResolvedValue({ data: stormGlassWeather3HoursFixture })
 
     const stormGlass = new StormGlass(axios)
     const response = await stormGlass.fetchPoints(lat, lng)
 
-    expect(response).toEqual({})
+    expect(response).toEqual(stormGlassNormalized3hoursFixture)
   })
 
 });
